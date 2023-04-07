82f56ff3205b6511f61a7d80ac49d70b9499823c
s_timokhin <s_timokhin@wargaming.net>
2023-04-07 18:31:11 +0300

    first

--- Source/geometryBase/Private/baseGeometryActor.cpp
+++ Source/geometryBase/Private/baseGeometryActor.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "baseGeometryActor.h";
+#include "baseGeometryActor.h"
 #include "Engine/Engine.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
@@ -8,7 +8,8 @@ DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
 // Sets default values
 AbaseGeometryActor::AbaseGeometryActor()
 {
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    // Set this actor to call Tick() every frame.
+    // You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 }
 
@@ -17,22 +18,10 @@ void AbaseGeometryActor::BeginPlay()
 {
     Super::BeginPlay();
 
-    FString Name = "John Connor";
+    BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("BaseMesh");
+    SetRootComponent(BaseMesh);
 
-    UE_LOG(LogBaseGeometry, Display, TEXT("Name: %s"), *Name)
-
-    int WeaponNum = 4;
-    float Health = 34.55;
-    bool isDead = false;
-
-    FString WeaponNumStr = "Weapons num = " + FString::FromInt(WeaponNum);
-    FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
-    FString IdDeadStr = "Is dead = " + FString(isDead ? "true" : "false");
-
-    FString Stat = FString::Printf(TEXT("\n == All Stat === \n %s \n %s \n %s"), *WeaponNumStr, *HealthStr, *IdDeadStr);
-    UE_LOG(LogBaseGeometry, Display, TEXT("%s"), *Stat);
-
-    GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, Name)
+    // printStringTypes();
     // printTypes();
 }
 
@@ -44,15 +33,26 @@ void AbaseGeometryActor::Tick(float DeltaTime)
 
 void AbaseGeometryActor::printTypes()
 {
-    int WeaponNum = 4;
-    int KillsNum = 7;
-    float Health = 34.55;
-    bool isDead = false;
-    bool HasWeapon = true;
+    UE_LOG(LogBaseGeometry, Warning, TEXT("Actor name: %s"), *GetName());
+    UE_LOG(LogBaseGeometry, Warning, TEXT("Weapons num: %d, kills num: %i"), WeaponNum, KillsNum);
+    UE_LOG(LogBaseGeometry, Warning, TEXT("Health: %f"), Health);
+    UE_LOG(LogBaseGeometry, Warning, TEXT("isDead: %d"), isDead);
+    UE_LOG(LogBaseGeometry, Warning, TEXT("Hasweapon: %d"), static_cast<int>(HasWeapon));
+}
 
-    UE_LOG(LogTemp, Display, TEXT("Weapons num: %d, kills num: %i"), WeaponNum, KillsNum);
-    UE_LOG(LogTemp, Display, TEXT("Health: %f"), Health);
-    UE_LOG(LogTemp, Display, TEXT("Health: %.2f"), Health);
-    UE_LOG(LogTemp, Display, TEXT("isDead: %d"), isDead);
-    UE_LOG(LogTemp, Display, TEXT("Hasweapon: %d"), static_cast<int>(HasWeapon));
+void AbaseGeometryActor::printStringTypes()
+{
+    FString Name = "John Connor";
+
+    UE_LOG(LogBaseGeometry, Warning, TEXT("Name: %s"), *Name)
+
+    FString WeaponNumStr = "Weapons num = " + FString::FromInt(WeaponNum);
+    FString HealthStr = "Health = " + FString::SanitizeFloat(Health);
+    FString IdDeadStr = "Is dead = " + FString(isDead ? "true" : "false");
+
+    FString Stat = FString::Printf(TEXT("\n == All Stat === \n %s \n %s \n %s"), *WeaponNumStr, *HealthStr, *IdDeadStr);
+    UE_LOG(LogBaseGeometry, Warning, TEXT("%s"), *Stat);
+
+    GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, Name);
+    GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Stat, true, FVector2D(1.5));
 }
--- Source/geometryBase/Public/baseGeometryActor.h
+++ Source/geometryBase/Public/baseGeometryActor.h
@@ -4,25 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
 #include "baseGeometryActor.generated.h"
 
 UCLASS()
 class GEOMETRYBASE_API AbaseGeometryActor : public AActor
 {
-  GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-  // Sets default values for this actor's properties
-  AbaseGeometryActor();
+    // Sets default values for this actor's properties
+    AbaseGeometryActor();
+
+    UPROPERTY(VisibleAnywhere)
+    UStaticMeshComponent* BaseMesh;
 
 protected:
-  // Called when the game starts or when spawned
-  virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditAnywhere, Category = "Weapon")
+    int32 WeaponNum = 4;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Stat")
+    int32 KillsNum = 7;
+
+    UPROPERTY(EditInstanceOnly, Category = "Health")
+    float Health = 34.55;
+
+    UPROPERTY(EditAnywhere, Category = "Health")
+    bool isDead = false;
+
+    UPROPERTY(VisibleAnywhere, Category = "Weapon")
+    bool HasWeapon = true;
 
 public:
-  // Called every frame
-  virtual void Tick(float DeltaTime) override;
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
 private:
-  void printTypes();
+    void printTypes();
+    void printStringTypes();
 };
